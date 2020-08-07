14762efd36dba39685adc9a9440aba123c416607
Clayton Blythe <claytonb@zillogroup.com>
2020-08-07 14:52:34 -0700

    simple change

--- fast_cli/fast_cli.go
+++ fast_cli/fast_cli.go
@@ -1,8 +1,67 @@
 package fast_cli
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
 
-func get_urls() []string {
+
+func Get_urls() {
 	fmt.Println("get_urls!")
+	js_url := get_js_url()
+	token := get_token(js_url)
+	fmt.Println(token)
+}
 
+func get_token(url string) string {
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	responseString := string(responseData)
+	token := GetStringInBetween(responseString, "{https:!0,endpoint:apiEndpoint,token:", ",urlCount:5")
+	token = token[1 : len(token)-1]
+	return token
+}
+
+func get_js_url() string {
+	url := "https://fast.com"
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	responseString := string(responseData)
+	out := GetStringInBetween(responseString, "<script src=", "></script>")
+	out = out[1 : len(out)-1]
+	s := []string{"https://fast.com", out}
+	js_url := strings.Join(s, "")
+	return js_url
+}
+func GetStringInBetween(str string, start string, end string) (result string) {
+	s := strings.Index(str, start)
+	if s == -1 {
+		return
+	}
+	s += len(start)
+	e := strings.Index(str, end)
+	if e == -1 {
+		return
+	}
+	return str[s:e]
 }
